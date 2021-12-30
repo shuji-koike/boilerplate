c68885e2ecd17b2a70f687e4d672c2fbe5eff095
Shuji Koike <shuji.koike@me.com>
2021-12-31 01:18:58 +0900

    Create publish.sh

---
+++ .github/publish.sh
@@ -0,0 +1,9 @@
+VERSION="${npm_package_version:-$(jq -r .version package.json)}"
+echo $VERSION
+for package in $( ls packages ); do
+  yarn --cwd packages/$package version --new-version $VERSION --no-git-tag-version
+done
+git commit -a -m "v$VERSION"
+git tag "v$VERSION"
+git push origin "v$VERSION"
+git tag -d "v$VERSION"
