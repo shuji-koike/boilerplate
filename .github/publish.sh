1f50dd45e47d079220389014e404db82df69037c
Shuji Koike <shuji.koike@me.com>
2022-01-15 00:20:00 +0900

    Update publish.sh

--- .github/publish.sh
+++ .github/publish.sh
@@ -4,6 +4,7 @@ for package in $( ls packages ); do
   yarn --cwd packages/$package version --new-version $VERSION --no-git-tag-version
 done
 git commit -a -m "v$VERSION"
+git push
 git tag "v$VERSION"
 git push origin "v$VERSION"
 git tag -d "v$VERSION"
